a18e16104dd13ddf4673f5ede55ea2dae8d1de32
Vinz Solano <bamharu228@gmail.com>
2026-03-19 20:39:15 +0800

    Photos

--- Lesson7/Activity/main.php
+++ Lesson7/Activity/main.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Activity</title>
+    <title>Book Information</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
